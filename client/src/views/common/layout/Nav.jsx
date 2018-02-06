3df0d9a6a35a60026edde03f834772fbb09e8f80
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-02-05 20:23:05 -0800

    Development branch

--- client/src/views/common/layout/Nav.jsx
+++ client/src/views/common/layout/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import khirosLogo from './khiros.png';
 
 const Nav = () => (
     <ul className="nav">
-        
+        <li className="nav-item">
+            <a className="nav-link active"><img src={khirosLogo} alt="Just something to stop error"  height="50" width="50"/></a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link">Link</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link">Link</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link">Disabled</a>
+        </li>
     </ul>
 )
 
