c291a4bf9beee926a654733038ce5b0a5a58d6ca
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-02-05 20:20:13 -0800

    Initial Point

--- client/src/views/common/layout/Nav.jsx
+++ client/src/views/common/layout/Nav.jsx
@@ -3,18 +3,7 @@ import React from 'react';
 
 const Nav = () => (
     <ul className="nav">
-        <li className="nav-item">
-            <a className="nav-link active"><img src="./khiros.png" alt="Just something to stop error"/></a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link">Link</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link">Link</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link">Disabled</a>
-        </li>
+        
     </ul>
 )
 
