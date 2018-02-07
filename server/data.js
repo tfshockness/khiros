50cb3b423226a4435acf0c3f5973c06309e93425
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-02-07 01:35:46 -0800

    Thinking about the data structure

---
+++ server/data.js
@@ -0,0 +1,92 @@
+export default {
+    company: {
+        name: "Khiros",
+        email: "atentimento@khiros.com",
+        logoImage: "",
+        phones: {
+            tel: "(69) 3212-1231",
+            cel: "(69) 3213-3123"
+        },
+        address: {
+            city: "Porto Velho",
+            province: "Rondonia",
+            country: "Brasil",
+            postalCode: "13250-526",
+            street: "Avenida do meio",
+            number: 4132
+        },
+        about: "This is the text about the company",
+        aboutImage: ""
+    },
+
+    carousel:[
+        {
+            id: 1,
+            image: "",
+            title: "Conheça nosso tratamento de quiropraxia",
+            link: {
+                label: "",
+                url: ""
+            }
+
+        },
+        {
+            id: 2,
+            image: "",
+            title: "Conheça nosso tratamento de quiropraxia",
+            link: {
+                label: "",
+                url: ""
+            }
+        }
+    ],
+
+    blog: [
+        {
+            id: 1,
+            image: "",
+            title: "My first Post",
+            subtitle: "Something that can goes more than just one phrase.",
+            tag: "Health",
+            text: "Loren ipsun :p",
+            socialMedia: {
+                facebook: "",
+                twitter: ""
+            }
+        }
+    ],
+
+    professionals: [
+        {
+            id: 1,
+            categoryName: "Doctor",
+            members: [
+                {
+                    id: 1,
+                    name: "Timoteo Shockness",
+                    speciality: "Developer",
+                    profileImg: "",
+                    credentials: "NO TEINS",
+                    bio: "Something about me and about my qualities as a professional"
+                }
+            ]
+        },
+
+        {
+            id: 2,
+            categoryName: "Physiotherapist",
+            members: [
+                {
+                    id: 2,
+                    name: "Yeeum Lee",
+                    speciality: "Scrum Master",
+                    profileImg: "",
+                    credentials: "CRA 132314",
+                    bio: "Loren is not a psun person"
+                }
+            ]
+        }
+    ]
+
+
+}
