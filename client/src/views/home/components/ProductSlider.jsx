7a4660c749266f3dc9619c1cbf44073d625e7238
Kate Yeeum <yeeumm@gmail.com>
2018-02-09 12:34:04 -0800

    Product slider - title added

--- client/src/views/home/components/ProductSlider.jsx
+++ client/src/views/home/components/ProductSlider.jsx
@@ -3,10 +3,14 @@ import spine1 from '../../../image/spine1.jpg';
 import spine2 from '../../../image/spine2.jpg';
 import backpain from '../../../image/backpain.jpg';
 
-export default class ProductSlider extends Component{
-    render(){
+ const ProductSlider = (props) => {
+   
         return(
             <div>
+                <div className="slider_title">
+                    <h1>NOSSOS <span className="font_normal">TRATAMENTOS</span></h1>
+                    <div className="slider_title_border"/>
+                </div> {/* .slider_title */}
                 <div id="productSlider" className="carousel slide" data-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
@@ -27,20 +31,20 @@ export default class ProductSlider extends Component{
                                     </div> {/* .productSlider_image */}
                                 </div> {/* .col */}
                             </div> {/* .row */}
-                            <a className="carousel-control-prev" href="#productSlider" role="button" data-slide="prev">
+                            <a className="carousel-control-prev productSlider_control" href="#productSlider" role="button" data-slide="prev">
                                 <span className="carousel-control-prev-icon productSlider_icon" aria-hidden="true"/>
                                 <span className="sr-only">Previous</span>
                             </a> {/* .carousel-prev */}
-                            <a className="carousel-control-next" href="#productSlider" role="button" data-slide="next">
+                            <a className="carousel-control-next productSlider_control" href="#productSlider" role="button" data-slide="next">
                                 <span className="carousel-control-next-icon productSlider_icon" aria-hidden="true"/>
                                 <span className="sr-only">Next</span>
                             </a> {/* .carousel-next */}
                         </div> {/* .carousel-item */}
                     </div> {/* .carousel-inner */}
-                    
-                    
                 </div> {/* .carousel */}
             </div>
         );
-    }
+    
 }
+
+export default ProductSlider;
