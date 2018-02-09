04f02660049ef0349ce69729ec5527b758769804
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-02-09 01:09:18 -0800

    Testing libraies for test

---
+++ server/test/sample.test.js
@@ -0,0 +1,7 @@
+var should = require('chai').should();
+
+describe("This is my sample test with Mocha and Chai", function(){
+    it("runs without problem.", function(){
+        true.should.be.a('boolean');
+    });
+});
