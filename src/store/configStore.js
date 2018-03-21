32d242e93168faa2ebf5e7efb90f55a2d6fd361c
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-03-20 22:29:18 -0700

    Configuring logger

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,6 +1,8 @@
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
 import { watchEntity } from '../sagas/mainSagas';
 import rootReducer from '../reducers'; //if this doesnt work, maybe its because of the index.js
 
@@ -12,12 +14,18 @@ export default function configureStore(initialState) {
         sagaMiddleware
     ];
 
+    if(process.env.NODE_ENV === 'development'){
+        middlewares.push(logger);
+    };
+
     const store = createStore(rootReducer, initialState, compose(
         applyMiddleware(...middlewares),
         window.devToolsExtension ? window.devToolsExtension() : f => f //Enable Redux Dev Tools
     ));
 
-    window.store = store;
+    if(process.env.NODE_ENV === 'development'){
+        window.store = store;
+    };
 
     //add WatchEntity later when create the sagas
     sagaMiddleware.run(watchEntity);
